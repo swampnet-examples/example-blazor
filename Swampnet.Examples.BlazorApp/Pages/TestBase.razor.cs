354870459ce50abdd23466cb1e6ea03bba645e1c
pj <petejw@theswamp.co.uk>
2019-10-23 21:34:45 +0100

    ....

---
+++ Swampnet.Examples.BlazorApp/Pages/TestBase.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Swampnet.Examples.BlazorApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swampnet.Examples.BlazorApp.Pages
+{
+    public class TestComponent : ComponentBase
+    {
+        [Inject] WeatherForecastService ForecastService { get; set; }
+
+        public string Message { get; set; } = "some message";
+
+        protected void DangerClick()
+        {
+        }
+    }
+}
