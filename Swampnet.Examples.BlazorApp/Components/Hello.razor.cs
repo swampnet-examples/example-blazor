743e24b912f634bda9dd38183256b3b0467417a1
pj <petejw@theswamp.co.uk>
2019-11-20 20:55:24 +0000

    ..

---
+++ Swampnet.Examples.BlazorApp/Components/Hello.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Swampnet.Examples.BlazorApp.Components
+{
+    public class HelloBase : ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+
+
+        protected void OnClick()
+        {
+            Title = DateTime.UtcNow.ToLongTimeString();
+        }
+
+    }
+}
